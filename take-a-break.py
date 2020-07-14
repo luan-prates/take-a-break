086d586dac17f01a8659703ec68437075d5dbcde
Luan Prates <pratesr21@gmail.com>
2020-07-14 11:16:53 -0300

    criando classe main
    criando classe main

--- take-a-break.py
+++ take-a-break.py
@@ -1,11 +1,19 @@
 import time
 import webbrowser
 
-total_breaks = 3
-break_cont = 0
+"""Módulo principal."""
 
-print("this program started on " + time.ctime())
-while(break_cont < total_breaks):
-    time.sleep(10)
-    webbrowser.open("https://indicadores.integrasus.saude.ce.gov.br/indicadores/indicadores-coronavirus/coronavirus-ceara")
-    break_cont = break_cont + 1
+def main():
+    """Função principal da aplicação."""
+    total_breaks = 3
+    break_cont = 0
+
+    print("Esse programa iniciou em: " + time.ctime())
+    while (break_cont < total_breaks):
+        time.sleep(108000)
+        webbrowser.open(
+            "https://indicadores.integrasus.saude.ce.gov.br/indicadores/indicadores-coronavirus/coronavirus-ceara")
+        break_cont = break_cont + 1
+
+if __name__ == "__main__":
+    main()
