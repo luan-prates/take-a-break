0e1b0c692a0838cd031521de8b81588990111f23
Luan Prates <pratesr21@gmail.com>
2020-07-14 08:09:49 -0300

    alteração do link
    alteração do link para o integraSUS

--- take-a-break.py
+++ take-a-break.py
@@ -7,5 +7,5 @@ break_cont = 0
 print("this program started on " + time.ctime())
 while(break_cont < total_breaks):
     time.sleep(10)
-    webbrowser.open("https://www.youtube.com/watch?v=y6Sxv-sUYtM")
+    webbrowser.open("https://indicadores.integrasus.saude.ce.gov.br/indicadores/indicadores-coronavirus/coronavirus-ceara")
     break_cont = break_cont + 1
